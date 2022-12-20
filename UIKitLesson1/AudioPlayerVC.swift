b5441557e0b8e857c247c4de8656babfa631e857
isaevk <kirill_isaev@aol.com>
2022-12-20 17:37:42 +0600

    home work 5

---
+++ UIKitLesson1/AudioPlayerVC.swift
@@ -0,0 +1,21 @@
+//
+//  AudioPlayerVC.swift
+//  UIKitLesson1
+//
+//  Created by Kirill Dev on 20.12.2022.
+//
+
+import UIKit
+import AVFoundation
+
+class AudioPlayerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+
+}
