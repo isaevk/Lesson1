4e51f0c9bbf946437224afedcfbbd3d95cfe57e6
Kirill Isaev <96496194+isaevk@users.noreply.github.com>
2022-12-20 17:25:02 +0600

    Revert "Create new home work"

---
+++ UIKitLesson1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitLesson1
+//
+//  Created by Kirill Dev on 06.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
